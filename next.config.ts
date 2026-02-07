a01b2dc66592bf401618d1d3b6e7ce8f0bae8fc6
pgplagrob <pgplagrob@gmail.com>
2026-02-07 11:25:39 +0700

    finsih

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/portforlio",
+  assetPrefix: "/portforlio/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
